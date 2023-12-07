a3cb5edcc3e849d20c9b89de326a54482f7584f8
soruse <piotrmolkontakt@gmail.com>
2023-12-07 17:32:48 +0100

    day 6 cleanup 🎀

--- src/days/6/puzzle.ts
+++ src/days/6/puzzle.ts
@@ -1,49 +1,31 @@
 const first = (input: string) => {
-  return '';
-  // function parseData(data: string) {
-  //   return data
-  //     .split(':')[1]
-  //     .split(/\s+/)
-  //     .filter((x) => x)
-  //     .map(Number);
-  // }
+  const [time, distance] = input.split(/\n/).map((l) =>
+    l
+      .split(': ')[1]
+      .split(' ')
+      .filter((x) => x)
+      .map(Number)
+  );
 
-  // const [tS, dS] = input.split(/\n/);
-  // const times = parseData(tS);
-  // const distances = parseData(dS);
+  let multiplied = 1;
 
-  // let multiplied = 1;
+  time.forEach((time, idx) => {
+    let ways = 0;
+    for (let i = 1; i <= time; i++) {
+      if (i * (time - i) > distance[idx]) {
+        ways++;
+      }
+    }
+    multiplied = multiplied * ways;
+  });
 
-  // times.forEach((time, idx) => {
-  //   let ways = 0;
-  //   for (let i = 1; i <= time; i++) {
-  //     if (i * (time - i) > distances[idx]) {
-  //       ways++;
-  //     }
-  //   }
-  //   multiplied = multiplied * ways;
-  // });
-
-  // return multiplied;
+  return multiplied;
 };
 
 const expectedFirstSolution = '440000';
 
 const second = (input: string) => {
-  function parseData(data: string) {
-    return Number(
-      data
-        .split(':')[1]
-        .split(/\s+/)
-        .filter((x) => x)
-        .join('')
-    );
-  }
-
-  const [tS, dS] = input.split(/\n/);
-  const time = parseData(tS);
-  const distance = parseData(dS);
-
+  const [time, distance] = input.split(/\n/).map((l) => Number(l.split(': ')[1].split(/\s+/).join('')));
   let ways = 0;
 
   for (let i = 1; i <= time; i++) {
